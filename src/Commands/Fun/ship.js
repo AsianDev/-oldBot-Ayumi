e233701f99d8aa8e79275a862244f434ab06b2fd
Ikigai <88756931+KaoriBotDev@users.noreply.github.com>
2022-02-03 20:33:33 +1000

    Fixed ship cmd

--- src/Commands/Fun/ship.js
+++ src/Commands/Fun/ship.js
@@ -1,6 +1,9 @@
 const Command = require('../../Handlers/Command.js')
 const Discord = require("discord.js");
-const Canvas = require('canvas')
+const block = "⬛"
+const heart = "🟥"
+const emotes = require("../../util/assets/Json/emotes.json")
+
 module.exports = new Command({
     name: "ship",
     description: "Ships you with someone",
@@ -9,35 +12,46 @@ module.exports = new Command({
     cooldown: 10000,
     type: "TEXT",
     async run(message, args, client) {
-        const canvas = Canvas.createCanvas(700, 250)
-        const ctx = canvas.getContext("2d")
-        const Target = message.mentions.users.first()
-        if(!Target) return message.channel.send("Please mention someone to ship yourself with.")
-        if(Target.id === message.author.id) return message.channel.send("Waa~~ you can not ship yourself with yourself 💔")
-        const bg = await Canvas.loadImage("https://cdn.discordapp.com/attachments/914003414273388544/914356300924747836/667336.jpg")
-       ctx.drawImage(bg, 0, 0, canvas.width, canvas.height) 
-       const avatar = await Canvas.loadImage(message.author.displayAvatarURL({format: 'png'}))
-        ctx.drawImage(avatar, 100, 25, 200, 200)
-        const TargetAvatar = await Canvas.loadImage(Target.displayAvatarURL({format: 'png'}))
-        ctx.drawImage(TargetAvatar, 400, 25, 200, 200)
-        const heart = await Canvas.loadImage("https://media.discordapp.net/attachments/914003414273388544/914359675082919996/heart-emoji.png?width=492&height=388")
-        const broken = await Canvas.loadImage("https://cdn.discordapp.com/attachments/914003414273388544/914359186035445780/photos-broken-heart-9.png")
-        const random = Math.floor(Math.random() * 99) +1
-        if(random >= 50) {
-            ctx.drawImage(heart, 275, 60, 150, 150)
-            const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'love.png')
-            const embed = new Discord.MessageEmbed()
-            .setDescription(`**:revolving_hearts: ${message.author.username} was shipped with ${Target.username} and they got %${random}**`)
-            .setImage(`attachment://love.png`)
-            .setColor("RANDOM")
-            return  message.channel.send({embeds: [embed], files: [attachment]})
-        } else {
-            ctx.drawImage(broken, 275, 60, 150, 150)
-            const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'love.png')
-            const embed = new Discord.MessageEmbed()
-            .setDescription(`**:revolving_hearts: ${message.author.username} was shipped with ${Target.username} and they got %${random}**`)
-            .setImage(`attachment://love.png`)
-            .setColor("RANDOM")
-            return message.channel.send({embeds: [embed], files: [attachment]})
-        }
-}})
+        const user = message.mentions.users.first();
+
+        const noUser = new Discord.MessageEmbed()
+        .setColor("RED")
+        .setTimestamp()
+        .setTitle(`${emotes.Error} MISSING ARGUEMENT`)
+        .setDescription("*Waa~* please mention a user to ship yourself with!")
+
+        if (!user) return message.channel.send({embeds: [noUser]})
+         if(user && user.id === message.author.id) {
+         return message.reply({embeds: [noUser]})
+         }
+    if (message.mentions.users.size < 2) {
+        let loveEmbed = new Discord.MessageEmbed()
+            .setColor('dd2e44')
+            .setTitle('Shipping...')
+            .setDescription(`Shipped ${message.author} and ${user}!`)
+            .setImage(`https://api.popcatdev.repl.co/ship?user1=${message.author.displayAvatarURL({ dynamic: false, format: "png" })}&user2=${user.displayAvatarURL({ dynamic: false, format: "png" })}`)
+            .addField(`**Ship Meter**`, ship())
+            
+       return message.channel.send({embeds: [loveEmbed]})
+
+    } else if (message.mentions.users.size > 1) {
+    let luv = new Discord.MessageEmbed()
+            .setColor('dd2e44')
+            .setTitle('Shipping...')
+            .setDescription(`Shipped ${message.mentions.users.first()} and ${message.mentions.users.last()}!`)
+            .setImage(`https://api.popcatdev.repl.co/ship?user1=${message.mentions.users.first().displayAvatarURL({ dynamic: false, format: "png" })}&user2=${message.mentions.users.last().displayAvatarURL({ dynamic: false, format: "png" })}`)
+            .addField(`**Ship Meter**`, ship())
+        message.channel.send({embeds: [luv]})
+    }
+    }
+    }
+
+)
+    function ship() {
+        const hearts = Math.floor(Math.random() * 110) + 0;
+        const hearte = (hearts/10)
+      
+        const str = `${heart.repeat(hearte)}${block.repeat(11 - hearte)} ${hearts}%`;
+        return str;
+    }
+
